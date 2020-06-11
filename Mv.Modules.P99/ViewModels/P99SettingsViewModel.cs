c89722c843121cc3f59ce6624f64e5a709fe9d14
JustinGZJ <524919547@qq.com>
2020-06-11 09:08:35 +0800

    Update P99SettingsViewModel.cs

--- Mv.Modules.P99/ViewModels/P99SettingsViewModel.cs
+++ Mv.Modules.P99/ViewModels/P99SettingsViewModel.cs
@@ -1,17 +1,7 @@
-﻿using Mv.Modules.P99.Service;
+﻿using Mv.Core.Interfaces;
 using Mv.Ui.Mvvm;
-using Prism.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Text;
-using System.Threading.Tasks;
-using Mv.Ui.Core;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using Unity;
-using Mv.Core.Interfaces;
 using Prism.Commands;
+using Unity;
 
 namespace Mv.Modules.P99.ViewModels
 {
@@ -19,17 +9,18 @@ namespace Mv.Modules.P99.ViewModels
     {
         private readonly IConfigureFile configureFile;
 
+        private DelegateCommand _saveConfig;
+
         public P99SettingsViewModel(IUnityContainer container, IConfigureFile configureFile) : base(container)
         {
             this.configureFile = configureFile;
-            Config = configureFile.GetValue<P99Config>(nameof(P99Config)) ?? new P99Config();          
+            Config = configureFile.GetValue<P99Config>(nameof(P99Config)) ?? new P99Config();
         }
 
         public P99Config Config { get; set; }
-
-        private DelegateCommand _saveConfig;
         public DelegateCommand SaveCommand =>
             _saveConfig ?? (_saveConfig = new DelegateCommand(Save));
+
         public void Save()
         {
             configureFile.SetValue<P99Config>(nameof(P99Config), Config);
