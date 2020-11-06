3a71814a14682afbf7336eaa2ec8fe5b9f8a7880
user <user@SZSZ-D6UNCTA42J>
2020-11-06 13:50:42 +0800

    1111

--- Mv.Modules.Axis/Configs/MotionConfig.cs
+++ Mv.Modules.Axis/Configs/MotionConfig.cs
@@ -6,23 +6,23 @@ namespace MotionWrapper
 
     public class MotionConfig
     {
-        [Category("输入")]
+        [Category("IO输入|输入")]
         [HeaderPlacement(HeaderPlacement.Collapsed)]
         public List<CInOutPrm> Inputs { get; private set; } = new List<CInOutPrm>();
 
-        [Category("输出")]
+        [Category("IO输出|输出")]
         [HeaderPlacement(HeaderPlacement.Collapsed)]
         public List<CInOutPrm> Outputs { get;private set; } = new List<CInOutPrm>();
 
-        [Category("轴参数")]
+        [Category("轴参数|轴参数")]
         [HeaderPlacement(HeaderPlacement.Collapsed)]
         public List<AxisParameter> AxisParameters { get; private set; } = new List<AxisParameter>();
 
-        [Category("坐标参数")]
+        [Category("坐标参数|坐标参数")]
         [HeaderPlacement(HeaderPlacement.Collapsed)]
         public List<CCrdPrm> CrdParameters { get; private set;} = new List<CCrdPrm>();
 
-        [Category("P2P参数")]
+        [Category("P2P参数|P2P参数")]
         [HeaderPlacement(HeaderPlacement.Collapsed)]
         public List<P2PPrm> P2PPrameters { get; private set; } = new List<P2PPrm>();
     }
