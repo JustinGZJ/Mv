3e9b87b11eb151fd844b2788c5a3edcefbe146b6
JustinGZJ <524919547@qq.com>
2020-12-21 20:45:25 +0800

    信息修改

--- MV.Core/Events/Class1.cs
+++ MV.Core/Events/Class1.cs
@@ -1,10 +1,12 @@
 ﻿using Prism.Events;
 using Prism.Logging;
+using System;
 
 namespace MV.Core.Events
 {
     public class UserMessage
     {
+        public DateTime Time { get; set; } = DateTime.Now;
         public string Source { get; set; }
         public Category Level { get; set; }
         public string Content { get; set; }
